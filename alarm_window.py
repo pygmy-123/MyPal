405461c3af5a5b4c304477ca28ed561715ff39a4
Qamar <qalkhatib0@gmail.com>
2021-11-28 12:21:38 +0200

    adding style

--- alarm_window.py
+++ alarm_window.py
@@ -18,39 +18,40 @@ root.geometry("1300x600")
 
 
 def open_alarm():
-    global rand1, rand2, myEntry, top
-    top = Toplevel()
-    top.title('Math game')
-    top.iconbitmap('icon.ico')
-    math1 = Label(top, text='Solve To Stop:   ' + str(rand1) + '+' + str(rand2), fg='#2F86A6',
+    global rand1, rand2, entry, top2
+    top2 = Toplevel()
+    top2.title('Math game')
+    # top2.iconbitmap('icon.ico')
+    math1 = Label(top2, text='Solve To Stop:   ' + str(rand1) + '+' + str(rand2), fg='#0D7377',
                   font=("Boulder", 20, "bold"))
     math1.grid(row=0, column=0, padx=30, pady=60)
-    myEntry = Entry(top, text="Answer Here      ")
-    myEntry.grid(row=1, column=0)
-    b2 = Button(top, text="Check The Answer", command=checker, padx=82, fg='white', bg='#2F86A6',
+    entry = Entry(top2, text="Answer Here      ")
+    entry.grid(row=1, column=0)
+    b2 = Button(top2, text="Check The Answer", command=checker, padx=82, fg='white', bg='#0D7377',
                 font=("Boulder", 12, "normal"))
     b2.grid(row=2, column=0, pady=30)
     mixer.init()
     mixer.music.load('Alarm-ringtone.mp3')
+    mixer.music.set_volume(0,1)
     mixer.music.play()
 
 
 def checker():
-    if str(rand1 + rand2) == myEntry.get():
-        math2 = Label(top, text='Well Done! Now We Know You Are Awake', fg='#4BB543',
+    if str(rand1 + rand2) == entry.get():
+        math2 = Label(top2, text='Well Done! Now We Know You Are Awake', fg='#4BB543',
                       font=("Boulder", 15, "normal"))
         math2.grid(row=3, column=0, padx=30, pady=60)
-        myEntry.delete(0, END)
-        b2 = Button(top, text="Check The Answer", command=checker, padx=82, fg='white', bg='#2F86A6', state=DISABLED,
+        entry.delete(0, END)
+        b2 = Button(top2, text="Check The Answer", command=checker, padx=82, fg='white', bg='#2F86A6', state=DISABLED,
                     font=("Boulder", 12, "normal"))
         b2.grid(row=2, column=0, pady=30)
         mixer.music.stop()
 
     else:
-        math2 = Label(top, text='Try Again! You Are Still Sleepy', fg='#FF0000',
+        math2 = Label(top2, text='Try Again! You Are Still Sleepy', fg='#FF0000',
                       font=("Boulder", 15, "normal"))
         math2.grid(row=3, column=0, padx=30, pady=60)
-        myEntry.delete(0, END)
+        entry.delete(0, END)
 
 
 def clock():
@@ -117,8 +118,9 @@ def clock():
     # label.grid(row=0,column=0)
     text_font = ("Boulder", 68, 'bold')
     # background = "#6096AA"
-    background = '#a3ccbe'
-    foreground = "black"
+    # background = '#a3ccbe'
+    background='#26AB95'
+    foreground = "#212121"
     border_width = 25
     label = Label(app_window, font=text_font, bg=background, fg=foreground, bd=border_width, pady=30, padx=45)
     label.grid(row=0, column=1)
@@ -131,8 +133,8 @@ def clock():
 
     digital_clock()
     Label(app_window, text="", font="Boulder 15 bold", bg='WhiteSmoke').grid(row=10, column=1)
-    Button(app_window, text="Set Alarm", font="Boulder 13 bold", command=threading, background='#e6dfaf',
-           activebackground='#e6dfaf').grid(row=11, column=1)
+    Button(app_window, text="Set Alarm",padx=40, font="Boulder 12 bold",fg='white' ,command=threading, background='#0D7377',
+           ).grid(row=11, column=1)
 
     def alarm():
         while True:
@@ -154,10 +156,10 @@ def clock():
                 Label(app_window, text="", font="Boulder 12 bold", bg='WhiteSmoke').grid(
                     row=17, column=1)
                 Label(app_window, text="OR", bg='WhiteSmoke', font="Boulder 12 bold").grid(row=19, column=1)
-                Button(app_window, padx=40, background='#e6dfaf',activebackground='#e6dfaf', text="Solve The Equation", font="Boulder 13 bold",
-                       command=open_alarm).grid(row=18, column=1)
-                Button(app_window, padx=40, text="Hand Gesture", font="Boulder 13 bold",
-                       background='#e6dfaf',activebackground='#e6dfaf', command=open_hand_gesture).grid(row=20, column=1)
+                Button(app_window, padx=40, background='#0D7377', text="Solve The Equation", font="Boulder 13 normal",
+                       fg='white',command=open_alarm).grid(row=18, column=1)
+                Button(app_window, padx=65, text="Hand Gesture", font="Boulder 13 normal", fg='white',
+                       background='#0D7377', command=open_hand_gesture).grid(row=20, column=1)
 
 
 def open_sleep():
@@ -173,12 +175,12 @@ rand1 = random.randrange(1, 50)
 rand2 = random.randrange(1, 50)
 root['bg'] = '#f5f5f5'
 
-blank_label1 = Label(root, text="Welcome to MyPal", fg='#396EB0', background='#f5f5f5', font=("Boulder", 22, "bold"))
+blank_label1 = Label(root, text="Welcome to MyPal", fg='#0D7377', background='#f5f5f5', font=("Boulder", 22, "bold"))
 blank_label1.grid(row=0, column=1, padx=30, pady=50)
-frame_left = LabelFrame(root, text="Alarm", padx=30, pady=30, fg='#396EB0', background='#f5f5f5',
+frame_left = LabelFrame(root, text="Alarm", padx=30, pady=30, fg='#0D7377', background='#f5f5f5',
                         font=("Boulder", 12, "normal"))
 frame_left.grid(row=3, column=0, padx=100)
-frame_right = LabelFrame(root, text="Sleep Detector", padx=30, pady=30, fg='#396EB0', background='#f5f5f5',
+frame_right = LabelFrame(root, text="Sleep Detector", padx=30, pady=30, fg='#0D7377', background='#f5f5f5',
                          font=("Boulder", 12, "normal"))
 frame_right.grid(row=3, column=2, padx=0)
 
@@ -194,8 +196,8 @@ img22 = ImageTk.PhotoImage(resize_image)
 label1 = Label(frame_right, image=img22)
 label1.grid(row=0, column=0)
 
-b1 = Button(root, text="Set Alarm", command=clock, padx=100, fg='black', bg='#396EB0', font=("Boulder", 12, "bold"))
-b2 = Button(root, text="Start Sleep Detector", command=open_sleep, padx=70, fg='black', bg='#396EB0',
+b1 = Button(root, text="Set Alarm", command=clock, padx=115, fg='white', bg='#0D7377', font=("Boulder", 12, "bold"))
+b2 = Button(root, text="Start Sleep Detector", command=open_sleep, padx=65, fg='white', bg='#0D7377',
             font=("Boulder", 12, "bold"))
 b1.grid(row=4, column=0, pady=50)
 b2.grid(row=4, column=2, pady=30)
